6774cc7054fd91d3ad4c0a05507d3ff2b85cd004
Rokeeb01Maruf <dayorokeeb234@gmail.com>
2026-05-15 09:50:27 +0100

    Created a connection to my cloud storage

--- backend/src/middleware/multer.ts
+++ backend/src/middleware/multer.ts
@@ -1,6 +1,9 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({})
-const upload = multer({ storage })
-
-export default upload;
+export const uploadSignup = multer({ 
+    storage,
+    limits: {
+        fileSize: 1*1024*1024
+    }
+})
--- backend/src/test/test.ts
+++ backend/src/test/test.ts
@@ -1,14 +1,20 @@
 import Router from "express";
-import upload from "../middleware/multer.js";
+import { uploadSignup } from "../middleware/multer.js";
 import cloudinary from "../config/cloudinary.js";
 
 const router = Router()
 
-router.post("/test", upload.single("image"), async(req, res)=>{
+router.post("/test", async(req, res)=>{
+    uploadSignup.single("image")(req, res, async(err)=>{
+        if(err){
+            if(err.code === "LIMIT_FILE_SIZE") return res.status(400).json({message: "Maximum file size exceeded"})
+            return res.status(400).json({message: err.code})
+        }
+    })
     try{
-        if(!req.file) return res.status(400).json({message: "Bad Request"})
+        if(!req.file) return res.status(400).json([{message: "Bad Request"}, req.file])
         const result = await cloudinary.uploader.upload(req.file.path)
-        return res.status(200).json([{mesage: "File uploaded successfully"}, result])
+        return res.status(200).json([{message: "File uploaded successfully"}, result])
     }catch(err){
         return res.status(500).json([{message: "server error, failed to upload file"}, err])
     }
